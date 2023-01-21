eeb8c431b5246c3f676d81ccf42a052dd10ee952
flucas <59033065+flucas96@users.noreply.github.com>
2023-01-21 21:11:18 +0100

    Add files via upload

---
+++ build/lib/streamlit_star_rating/frontend/build/precache-manifest.a982ecc6e1bfb804d4fb37038240d2af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb1f82a61f4b321d0129876a0ba9541c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d8575c6ea9ca83489dd6",
+    "url": "./static/js/2.76da4152.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.76da4152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "408fbcd9d52d14dd784a",
+    "url": "./static/js/main.6fe35020.chunk.js"
+  },
+  {
+    "revision": "5136ac6826bacddd77a8",
+    "url": "./static/js/runtime-main.044ed49b.js"
+  }
+]);
